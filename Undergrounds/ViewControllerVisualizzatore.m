ec0b44eba15d5bd9ab94cbaa3529fd117954781c
xbomber360 <siciliano@MacBook-Pro-di-LORENZO.local>
2015-03-24 19:50:46 +0100

    aggiunto scroller al visualizzare ma da fixare bene

--- Undergrounds/ViewControllerVisualizzatore.m
+++ Undergrounds/ViewControllerVisualizzatore.m
@@ -20,9 +20,27 @@
     [super viewDidLoad];
 
     
-    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(10, 10, 400, 800)];
+    UIWebView *webView = [[UIWebView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
     [webView loadData:mappaDaVisualizzare MIMEType:@"application/pdf" textEncodingName:@"utf-8" baseURL:nil];
-    [self.view addSubview:webView];
+    //[webView setBackgroundColor:[UIColor clearColor]];
+    webView.scalesPageToFit= true;
+   // [webView stringByEvaluatingJavaScriptFromString:@"document.body.style.zoom = 50.0;"];
+    webView.contentMode = UIViewContentModeScaleAspectFit;
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame: self.view.frame];
+    scrollView.contentMode = UIViewContentModeScaleAspectFit;
+    scrollView.autoresizingMask = ( UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
+    scrollView.maximumZoomScale = 2.0;
+    scrollView.minimumZoomScale = 1;
+    scrollView.clipsToBounds = YES;
+    //scrollView.delegate = self;
+    [scrollView addSubview:webView];
+    //int scroll=500; //Pixels to scroll
+//    NSString* s=[[NSString alloc] initWithFormat:@"window.scrollTo(0, %i)",scroll];
+//    [webView stringByEvaluatingJavaScriptFromString:s];
+//
+    [self.view addSubview:scrollView];
+
+    [scrollView setZoomScale:5.0];
 
 }
 
